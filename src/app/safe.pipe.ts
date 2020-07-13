4a8a105ec66df985108914ed00a9bfd5733ef748
Chung-Yang Li <t0912398031@gmail.com>
2020-07-13 12:23:06 -0700

    safe pipe

--- src/app/safe.pipe.ts
+++ src/app/safe.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
 
 @Pipe({
   name: 'safe'
@@ -7,8 +7,19 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class SafePipe implements PipeTransform {
 
   constructor(private sanitizer: DomSanitizer){}
-  transform(url:any) {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  // transform(url:any) {
+  //   return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  // }
+
+  public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+    switch (type) {
+			case 'html': return this.sanitizer.bypassSecurityTrustHtml(value);
+			case 'style': return this.sanitizer.bypassSecurityTrustStyle(value);
+			case 'script': return this.sanitizer.bypassSecurityTrustScript(value);
+			case 'url': return this.sanitizer.bypassSecurityTrustUrl(value);
+			case 'resourceUrl': return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+			default: throw new Error(`Invalid safe type specified: ${type}`);
+		}
   }
 
 }
